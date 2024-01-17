297381b9649c3a6a3d6ea12bb9fcfce13bd23061
magersoft <magervlad@yandex.ru>
2024-01-17 19:47:03 +0200

    WelcomeInfo component

--- src/modules/auth/ui/components/WelcomeInfo/WelcomeInfo.vue
+++ src/modules/auth/ui/components/WelcomeInfo/WelcomeInfo.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div :class="$style['welcome-info']">Magermoney</div>
+	<div :class="$style['welcome-info']">
+		<div :class="$style['welcome-info__logo']">
+			<div
+				v-for="i in 6"
+				:key="i"
+				:class="[$style['layer'], $style[`layer-${i}`]]"
+			/>
+		</div>
+		<h1 :class="$style['welcome-info__title']">MagerMoney</h1>
+	</div>
 </template>
 
 <style module lang="scss">
